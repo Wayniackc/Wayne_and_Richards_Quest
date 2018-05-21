e8cdd8173f98bace92717a7ba87c441a8dd72956
Wayniackc <37186292+Wayniackc@users.noreply.github.com>
2018-05-21 18:07:51 -0500

    Revert "Finished Herald's opening scene"
    This reverts commit 1d7c6e685df14b64e4643795438d75702f3bb0b2.

--- Scripts/Courtroom.gd
+++ Scripts/Courtroom.gd
@@ -1,9 +1,7 @@
 # Courtroom.gd
 extends RichTextLabel
 
-onready var panelNode2 = get_parent()
-
-var dialog = ["Prisoner!", "You stand accused of crimes against the realm.", "Come forward and face your king!"]
+var dialog = ["Prisoner! You stand accused of crimes against the realm.", "Come forward and face your king!"]
 var page = 0
 
 
@@ -11,19 +9,14 @@ func _ready():
 	set_bbcode(dialog[page])
 	set_visible_characters(0)
 	set_process_input(true)
-	panelNode2.hide()
-		
+	
 func _input(event):
-	if Input.is_action_pressed("ui_accept"):
+	if event is InputEventMouseButton && event.is_pressed():
 		if get_visible_characters() > get_total_character_count():
 			if page < dialog.size() - 1:
 				page += 1
 				set_bbcode(dialog[page])
 				set_visible_characters(0)
-			else:
-				panelNode2.hide()
-		else:
-			set_visible_characters(get_total_character_count())
 
 func _on_Timer_timeout():
 	set_visible_characters(get_visible_characters() + 1)
