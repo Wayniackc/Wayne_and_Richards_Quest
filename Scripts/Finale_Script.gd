800e1b4a0e709717089412d49a8b14382835368b
Wayniackc <37186292+Wayniackc@users.noreply.github.com>
2018-06-07 21:08:02 -0500

    Added fade out animation for scene transition

--- Scripts/Finale_Script.gd
+++ Scripts/Finale_Script.gd
@@ -3,6 +3,18 @@ extends RichTextLabel
 onready var panelNode = get_parent()
 onready var timer = get_node("Timer")
 
+onready var wizard_tween = get_node("../../../Wizard/AnimatedSprite/Tween")
+onready var wizard = get_node("../../../Wizard/AnimatedSprite")
+
+onready var king_tween = get_node("../../../King/Sprite/Tween")
+onready var king = get_node("../../../King/Sprite")
+
+onready var herald_tween = get_node("../../../Herald/AnimationPlayer/Sprite/Tween")
+onready var herald = get_node("../../../Herald/AnimationPlayer/Sprite")
+
+onready var bg_tween = get_node("../../../Background/Tween")
+onready var bg = get_node("../../../Background")
+
 var dialog = ["Herald: \"Nasty piece of business, Your Grace. We are better off having rid the realm of that scum.\"", "King: \"I fear it won't be the last we've heard of him if he's anything like his father.\"", "Wizard: \"He is exactly like his father to be sure.\"", "King: \"That should keep us all awake at night.\"", "Wizard: \"If you're having trouble sleeping, Your Grace, I can make you a sleeping potion that will assuredly help?\"", "King: \"Oh for God's sake. I'm going back to my chambers before I send the lot of you to the wasteland.\""]
 var page = 0
 
@@ -23,18 +35,24 @@ func _input(event):
 				set_visible_characters(0)
 			else:
 				panelNode.hide()
-#				_banished()
+				_fade_out()
 		else:
 			set_visible_characters(get_total_character_count())
 
 func _on_Timer_timeout():
 	set_visible_characters(get_visible_characters() + 1)
 	
-func _banished():
-	if can_talk == true:
-		can_talk = false
-		get_node("../../../MC/AnimationPlayer/AnimatedMC").play("Banished")
-
 func _on_Timer2_timeout():
 	timer.start()
 	panelNode.show()
+	
+func _fade_out():
+	wizard_tween.interpolate_property(wizard, "modulate", Color(1, 1, 1, 1), Color(1, 1, 1, 0), 2.0, Tween.TRANS_LINEAR, Tween.EASE_IN)
+	wizard_tween.start()
+	king_tween.interpolate_property(king, "modulate", Color(1, 1, 1, 1), Color(1, 1, 1, 0), 2.0, Tween.TRANS_LINEAR, Tween.EASE_IN)
+	king_tween.start()
+	herald_tween.interpolate_property(herald, "modulate", Color(1, 1, 1, 1), Color(1, 1, 1, 0), 2.0, Tween.TRANS_LINEAR, Tween.EASE_IN)
+	herald_tween.start()
+	yield(get_tree().create_timer(3.0), "timeout")
+	bg_tween.interpolate_property(bg, "modulate", Color(1, 1, 1, 1), Color(1, 1, 1, 0), 2.0, Tween.TRANS_LINEAR, Tween.EASE_IN)
+	bg_tween.start()
